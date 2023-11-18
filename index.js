b619e544076751558f279c62fe9ae94af2232a43
Saadi-Yip <saadshabbir@leilanitech.com>
2023-11-18 18:32:18 +0500

    changing folder structuing

--- index.js
+++ index.js
@@ -19,6 +19,26 @@ const io = socketIO(server, {
 });
 const PORT = process.env.PORT || 5000;
 
+const cors = require("cors");
+
+// Middleware for parsing JSON
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+let corsOptions = {
+  origin: ["http://localhost:3000", "*"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 200,
+  allowedHeaders: ["Content-Type", "Authorization"], // Add any other headers you need
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+// Middleware for handling file uploads (images)
+
 // Socket.IO logic
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
