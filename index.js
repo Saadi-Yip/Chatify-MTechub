998d671955ff85cace0feb38d19991ef8fb6fd5c
Saadi-Yip <saadshabbir@leilanitech.com>
2023-11-18 10:35:52 +0500

    Changing Auth Status

--- index.js
+++ index.js
@@ -238,6 +238,18 @@ app.get("/messages/:userId", authenticateUser, async (req, res) => {
     res.status(500).json({ error: `Internal server error, ${error.message}` });
   }
 });
+// Get all messages for the logged-in user
+app.get("/messages", authenticateUser, async (req, res) => {
+  try {
+    const messages = await Message.find({
+      $or: [{ sender: req.userId }, { receiver: req.userId }],
+    }).sort({ timestamp: 1 });
+
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ error: `Internal server error, ${error.message}` });
+  }
+});
 
 // Start the server
 server.listen(PORT, () => {
